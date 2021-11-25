f376843be4cc141d794b78d1811e032a4f998124
Duncan <81414673+pilijiaotun@users.noreply.github.com>
2021-11-26 02:56:33 +0800

    Add files via upload
    use dp to find the maximum value when it cross the matrix

---
+++ dpnote1.py
@@ -0,0 +1,15 @@
+# 这是一个示例 Python 脚本。
+
+# 按 Shift+F10 执行或将其替换为您的代码。
+# 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
+
+
+class Solution:
+    def maxValue(self, grid: List[List[int]]) -> int:
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if i == 0 and j == 0: continue
+                if i == 0: grid[i][j] += grid[i][j - 1]
+                elif j == 0: grid[i][j] += grid[i - 1][j]
+                else: grid[i][j] += max(grid[i][j - 1], grid[i - 1][j])
+        return grid[-1][-1]
